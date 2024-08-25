b0f9feebe98d15ebc01eb3c83296f142ad9a1367
anurag1302 <anurag3487@gmail.com>
2024-08-26 00:02:48 +0530

    tweaks

--- vars/vars.go
+++ vars/vars.go
@@ -2,16 +2,16 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
 	var name string = "John" //explicit declaration & assignment
-	var age = 38 // implicit one, compiler infers
+	var age = 38             // implicit one, compiler infers
 	var isAdmin bool = true
-	percentage:= 98.8 //shorthand notation
+	percentage := 98.8 //shorthand notation
 
 	var interestRate float32 = 10.45
 	fmt.Println(name)
 	fmt.Println(age)
 	fmt.Println(isAdmin)
 	fmt.Println(percentage)
-	fmt.Println("The interest rate is = ",interestRate+10)
+	fmt.Println("The interest rate is = ", interestRate+10)
 }
