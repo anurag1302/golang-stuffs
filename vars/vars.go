a20b2f5f95dd15799c42d0638757126c661fcdad
anurag1302 <anurag3487@gmail.com>
2024-08-25 23:49:17 +0530

    variables in go

---
+++ vars/vars.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main(){
+	var name string = "John"
+	var age = 38
+	var isAdmin bool = true
+	percentage:=98.8
+
+	var interestRate float32=10.45
+	fmt.Println(name)
+	fmt.Println(age)
+	fmt.Println(isAdmin)
+	fmt.Println(percentage)
+	fmt.Println("The interest rate is = ",interestRate+10)
+}
